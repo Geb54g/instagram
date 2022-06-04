1e12078dc25218aeffbde3b2406e59ea46f67b2a
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-06-04 20:58:45 +0300

    ft:created a superuser

--- instagram/instagramApp/models.py
+++ instagram/instagramApp/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class Post (models.Model):
-    author = models.ForeignKey('author.User',on_delete=models.CASCADE)
+class post (models.Model):
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     image = models.ImageField(blank=True,null=True)
     caption=models.TextField()
     create_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.caption
