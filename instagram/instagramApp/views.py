b6506a000f90bd2af337ab10494f436feb2a53ef
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-06-04 21:21:38 +0300

    ft:created a template to display the html files

--- instagram/instagramApp/views.py
+++ instagram/instagramApp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from .models import post
-
+from django.views.generic import(
+    ListView
+)
 # Create your views here.
+
+class postListView(ListView):
+    template_name="instagramAPP/post.html"
+    queryset=post.objects.all()
+    context_object_name='posts'
