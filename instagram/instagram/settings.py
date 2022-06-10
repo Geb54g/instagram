fc64e84841f1a351fd6efe11565601177d843695
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-06-10 16:57:21 +0300

    bg:fixed a bug

--- instagram/instagram/settings.py
+++ instagram/instagram/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL ='/media/'
 
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME':'gebs',
     'API_KEY':'364344722915987',
